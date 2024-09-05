546f3d10091b5d040fdc9134e97d0f63847897da
shrinivas mada <shreemada@gmail.com>
2024-09-05 20:31:32 +0530

    sql script and .csv files for testing

---
+++ sql/schema.sql
@@ -0,0 +1,15 @@
+﻿CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    EmployeeName NVARCHAR(100),
+    EmailID NVARCHAR(100),
+    Designation NVARCHAR(100)
+);
+
+GO
+
+CREATE TABLE Departments (
+    DepartmentID INT PRIMARY KEY,
+    DepartmentName NVARCHAR(100),
+    Location NVARCHAR(100),
+    Phone NVARCHAR(15)
+);
